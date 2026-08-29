5593f1c20033ebf4af65e0ddc00c5be22a2b5c80
Jacob Brown <brown.jacob.alexander@gmail.com>
2026-08-28 18:03:53 -0600

    function to display all todo items

--- src/dependencies/dom.js
+++ src/dependencies/dom.js
@@ -1,2 +1,18 @@
 // module to manipulate the DOM
+import { TODOS } from "./todo.js";
 
+const content = document.querySelector(".content");
+
+const populateToDos = (function () {
+    const displayAll = function () {
+        TODOS.forEach((item) => {
+            const toDoNode = document.createElement("p");
+            toDoNode.className = "to-do-item";
+            toDoNode.id = item.id;
+            toDoNode.textContent = item.title;
+            content.addChild(toDoNode);
+        })
+    }
+})();
+
+export { populateToDos }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import "./style.css";
 import * as toDo from "./dependencies/todo.js";
 import * as project from "./dependencies/project.js";
 import * as dom from "./dependencies/dom.js";
@@ -9,3 +10,5 @@ toDo.addToDo("title 2", "description 2", "dueDate 2", 2, true, "not false");
 console.log(toDo.TODOS);
 console.log(project.PROJECTS);
 console.log(toDo.getToDoItemsByProject(null));
+
+dom.populateToDos.displayAll();
