3956d23a92f9fe19a450ac541556cc8271da8d45
Jacob Brown <brown.jacob.alexander@gmail.com>
2026-08-31 14:01:48 -0600

    use inline svg

--- src/dependencies/dom.js
+++ src/dependencies/dom.js
@@ -1,6 +1,19 @@
 // module to manipulate the DOM
-import { TODOS } from "./todo.js";
+import { TODOS, deleteToDo } from "./todo.js";
 import { PROJECTS } from "./project.js";
+
+function createSVGIcon(path, className) {
+    const svgIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svgIcon.setAttribute("viewBox", "0 0 24 24");
+    svgIcon.setAttribute("class", className);
+
+    const svgPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    svgPath.setAttribute("d", path);
+    
+    svgIcon.appendChild(svgPath);
+
+    return svgIcon;
+}
 
 const todoContainer = document.querySelector(".todo-container");
 const projectContainer = document.querySelector(".project-container");
@@ -47,15 +60,25 @@ const populateToDos = (function () {
             todoDate.className = "todo-date";
             todoDate.textContent = item.dueDate;
 
+            const todoEdit = createSVGIcon("M10 20H6V4H13V9H18V12.1L20 10.1V8L14 2H6C4.9 2 4 2.9 4 4V20C4 21.1 4.9 22 6 22H10V20M20.2 13C20.3 13 20.5 13.1 20.6 13.2L21.9 14.5C22.1 14.7 22.1 15.1 21.9 15.3L20.9 16.3L18.8 14.2L19.8 13.2C19.9 13.1 20 13 20.2 13M20.2 16.9L14.1 23H12V20.9L18.1 14.8L20.2 16.9Z", "todo-edit");
+            const todoDelete = createSVGIcon("M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z", "todo-delete")
+
             checkedButton.addEventListener("click", function(){
                 item.checked = !item.checked;
                 checkedButton.checked = item.checked;
+            })
+
+            todoDelete.addEventListener("click", function() {
+                deleteToDo(item.id);
+                displayByFilter(filterParam, filterValue);
             })
 
             toDoDiv.appendChild(checkedButton);
             toDoDiv.appendChild(todoTitle);
             toDoDiv.appendChild(todoDescription);
             toDoDiv.appendChild(todoDate);
+            toDoDiv.appendChild(todoEdit);
+            toDoDiv.appendChild(todoDelete);
             todoContainer.appendChild(toDoDiv);
         })    
     }
