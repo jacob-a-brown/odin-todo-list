152573fba5fb610424d1f7519fda8923c4e69767
Jacob Brown <brown.jacob.alexander@gmail.com>
2026-08-31 18:29:01 -0600

    add scroll bar to overflow text in div

--- src/dependencies/dom.js
+++ src/dependencies/dom.js
@@ -61,6 +61,9 @@ const populateToDos = (function () {
             checkedButton.name = item.id;
             checkedButton.checked = item.checked;
 
+            const todoTextDiv = document.createElement("div");
+            todoTextDiv.className = "todo-text-div"
+
             const todoTitle = document.createElement("h3");
             todoTitle.className = "todo-title";
             todoTitle.textContent = item.title;
@@ -72,6 +75,10 @@ const populateToDos = (function () {
             const todoDate = document.createElement("p");
             todoDate.className = "todo-date";
             todoDate.textContent = item.dueDate;
+
+            todoTextDiv.appendChild(todoTitle);
+            todoTextDiv.appendChild(todoDescription);
+            todoTextDiv.appendChild(todoDate);
 
             const todoEdit = createSVGIcon("M10 20H6V4H13V9H18V12.1L20 10.1V8L14 2H6C4.9 2 4 2.9 4 4V20C4 21.1 4.9 22 6 22H10V20M20.2 13C20.3 13 20.5 13.1 20.6 13.2L21.9 14.5C22.1 14.7 22.1 15.1 21.9 15.3L20.9 16.3L18.8 14.2L19.8 13.2C19.9 13.1 20 13 20.2 13M20.2 16.9L14.1 23H12V20.9L18.1 14.8L20.2 16.9Z", "todo-edit");
             const todoDelete = createSVGIcon("M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z", "todo-delete")
@@ -193,9 +200,7 @@ const populateToDos = (function () {
             });
 
             toDoDiv.appendChild(checkedButton);
-            toDoDiv.appendChild(todoTitle);
-            toDoDiv.appendChild(todoDescription);
-            toDoDiv.appendChild(todoDate);
+            toDoDiv.appendChild(todoTextDiv);
             toDoDiv.appendChild(todoEdit);
             toDoDiv.appendChild(todoDelete);
             todoContainer.appendChild(toDoDiv);
